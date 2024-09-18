00b5062331e5e40c08047b3a65eb869b0809c5bc
rafaelreis8000 <priv.rafa.reis@gmail.com>
2024-09-18 08:35:19 -0300

    .

---
+++ POO2 - Quarta/Matriz/Matriz/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matriz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Definindo a matriz de nomes com cinco nomes padrão
+            string[] nomes = { "Zeca", "Jorge", "Piririu", "Robert", "Mihan" };
+
+            // Exibindo os nomes usando a estrutura foreach
+            Console.WriteLine("Nomes da lista:");
+            foreach (string nome in nomes)
+            {
+                Console.WriteLine(nome);
+            }
+
+            // Exibindo os nomes em maiúsculas usando ToUpper()
+            Console.WriteLine("\nNomes em maiúsculas:");
+            foreach (string nome in nomes)
+            {
+                Console.WriteLine(nome.ToUpper());
+            }
+        }
+    }
+}
